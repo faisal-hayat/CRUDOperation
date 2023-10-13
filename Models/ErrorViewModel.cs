324cd0d6c5a7f88ba7691f31a8fa8a8121d7fad8
M Faisal Hayat <m.faisalhayat3@gmail.com>
2023-10-13 18:48:03 +0500

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRUD_Operations.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
