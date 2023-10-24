583e9bc8d99c3b036d8fa554f6f03c202b844ad2
M Faisal Hayat <m.faisalhayat3@gmail.com>
2023-10-24 23:15:25 +0500

    identity has been added

--- Program.cs
+++ Program.cs
@@ -52,7 +52,6 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthentication();;
 app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
