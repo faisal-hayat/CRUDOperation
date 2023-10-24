8b39f1a7312c99009427fea49a61ad2fe76611f7
M Faisal Hayat <m.faisalhayat3@gmail.com>
2023-10-24 22:01:05 +0500

    program.cs has been added

--- Program.cs
+++ Program.cs
@@ -5,28 +5,37 @@ using Microsoft.AspNetCore.Identity;
 using CRUD_Operations.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 #region add sql server context
-
 // This is where we will be adding the sql server service
 builder.Services.AddDbContext<CRUD_Operations.Data.ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
 ));
 
-
 builder.Services.AddDbContext<CRUD_OperationsContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 #endregion
 
+
+#region configure identity 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<CRUD_OperationsContext>();
-
 // repository services will be added here
 builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 2;
+    options.Password.RequireDigit = false;
+    options.Password.RequiredUniqueChars = 0;
+    options.SignIn.RequireConfirmedEmail = false;
+});
+#endregion
 
 var app = builder.Build();
 
@@ -40,14 +49,14 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
 app.UseAuthentication();;
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+// following line need to be added
+app.MapRazorPages();
 app.Run();
